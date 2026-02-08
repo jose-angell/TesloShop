cbd12405d1e02bcb7aa4b8abaccb79107f479981
jose Angel <josegallardo.cor@gmail.com>
2026-02-07 21:55:59 -0600

    feat: paginacion y productos

--- src/shop/pages/home/HomePages.tsx
+++ src/shop/pages/home/HomePages.tsx
@@ -1,9 +1,14 @@
+import { CustomPagination } from '@/components/custom/CustomPagination'
+import { products } from '@/mocks/products.mock'
 import { CustomJumbotron } from '@/shop/components/CustomJumbotron'
+import { ProductsGrid } from '@/shop/components/ProductsGrid'
 
 export const HomePages = () => {
   return (
     <>
-    <CustomJumbotron title='Todos los Productos' />
+      <CustomJumbotron title='Todos los Productos' />
+      <ProductsGrid products={products} />
+      <CustomPagination totalPages={7} />
     </>
   )
 }
