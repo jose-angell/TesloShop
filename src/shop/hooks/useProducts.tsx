bac39ec85611cd2d3a890634b66a630ba3311ae3
jose Angel <josegallardo.cor@gmail.com>
2026-02-17 22:24:03 -0600

    feat: paginacion de productos

--- src/shop/hooks/useProducts.tsx
+++ src/shop/hooks/useProducts.tsx
@@ -1,13 +1,22 @@
 import { useQuery } from "@tanstack/react-query"
 import { getProductsAction } from "../actions/get-products.action";
+import { useSearchParams } from "react-router";
 
 
 
 export const useProducts = () => {
-// TODO: falta logica
+  const [searchParams] = useSearchParams();
+  const limit = searchParams.get('limit') || 9;
+  const page = searchParams.get('offset') || 1;
+
+  const offset = Number(page) - 1 * Number(limit);
 
   return useQuery ({
-    queryKey: ['productos'],
-    queryFn: getProductsAction
+    queryKey: ['productos', {offset, limit}],
+    queryFn: () => getProductsAction({
+      limit: isNaN(+limit) ? 9 : limit, 
+      offset: isNaN(offset) ? 0 : offset,
+    }),
+    staleTime: 1000 * 60 * 5, // 5 minutos se matiene vigente la data, no se vuelve a cargar
   });
 }
