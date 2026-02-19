0a246c95130220f506ed3e3c52e1d76801a46df8
jose Angel <josegallardo.cor@gmail.com>
2026-02-18 23:23:00 -0600

    feat: filtro por rango de precios

--- src/shop/hooks/useProducts.tsx
+++ src/shop/hooks/useProducts.tsx
@@ -5,19 +5,47 @@ import { useParams, useSearchParams } from "react-router";
 
 
 export const useProducts = () => {
+  const {gender} = useParams();
   const [searchParams] = useSearchParams();
   const limit = searchParams.get('limit') || 9;
   const page = searchParams.get('offset') || 1;
   const sizes = searchParams.get('sizes') || undefined;
   const offset = Number(page) - 1 * Number(limit);
-  const {gender} = useParams();
+
+  const price = searchParams.get('price') || 'any';
+  let minPrice = undefined;
+  let maxPrice = undefined;
+
+  switch(price){
+    case 'any':
+      break;
+    case '0-50':
+      minPrice = 0;
+      maxPrice = 50;
+      break;
+    case '50-100':
+      minPrice = 50;
+      maxPrice = 100;
+      break;
+    case '100-200':
+      minPrice = 100;
+      maxPrice = 200;
+      break;
+    case '200+':
+      minPrice = 200;
+      maxPrice = undefined;
+      break;
+  }
+
   return useQuery ({
-    queryKey: ['productos', {offset, limit, sizes, gender}],
+    queryKey: ['productos', {offset, limit, sizes, gender, minPrice, maxPrice}],
     queryFn: () => getProductsAction({
       limit: isNaN(+limit) ? 9 : limit, 
       offset: isNaN(offset) ? 0 : offset,
       sizes,
       gender,
+      minPrice,
+      maxPrice
     }),
     staleTime: 1000 * 60 * 5, // 5 minutos se matiene vigente la data, no se vuelve a cargar
   });
