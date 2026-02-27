a2a6288a27f37b1b689694715d955bab7f0acf3c
jose Angel <josegallardo.cor@gmail.com>
2026-02-26 21:38:18 -0600

    feat: interceptores

--- src/api/tesloApi.ts
+++ src/api/tesloApi.ts
@@ -5,6 +5,12 @@ const tesloApi = axios.create({
 })
 
 
-//TODOD: interceptiores
+tesloApi.interceptors.request.use( (config) => {
+    const token = localStorage.getItem('token');
+    if(token){
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+})
 
 export { tesloApi}
