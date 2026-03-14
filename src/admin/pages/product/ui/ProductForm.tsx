b1cf14ca86a8e20b3b34e04334965353b04c92f9
jose Angel <josegallardo.cor@gmail.com>
2026-03-13 23:43:05 -0600

    feat: agregar y eliminar tallas y etiquetas

--- src/admin/pages/product/ui/ProductForm.tsx
+++ src/admin/pages/product/ui/ProductForm.tsx
@@ -1,8 +1,8 @@
 import { AdminTitle } from '@/admin/Components/AdminTitle';
 import { Button } from '@/components/ui/button';
 import type { Product, Size } from '@/interfaces/product.interface';
-import { X, SaveAll, Tag,  Upload } from 'lucide-react';
-import { useState } from 'react';
+import { X, SaveAll, Tag,  Upload, Plus } from 'lucide-react';
+import { useRef, useState } from 'react';
 import { Link } from 'react-router';
 import { useForm } from 'react-hook-form';
 import { cn } from '@/lib/utils';
@@ -23,20 +23,22 @@ export const ProductForm = ({ title, subTitle, product }: Props) => {
 
   });
 const selectSizes = watch('sizes')
-//Todo: remover despuies
-const onSubmit = (prodictLike: Product) => {
-  
-}
+const selectTags = watch('tags')
+const currentStock = watch('stock');
+
+  const labelInputRef = useRef<HTMLInputElement>(null);
   const addTag = () => {
-    if (newTag.trim() && !product.tags.includes(newTag.trim())) {
-      // setProduct((prev) => ({
-      //   ...prev,
-      //   tags: [...prev.tags, newTag.trim()],
-      // }));
-    }
+    const newTag = labelInputRef.current!.value;
+    if(newTag === '') return;
+    const newTagSet = new Set(getValues('tags'));
+    newTagSet.add(newTag);
+    setValue('tags', Array.from(newTagSet));
   };
 
-  const removeTag = (tagToRemove: string) => {
+  const removeTag = (tag: string) => {
+    const newTagSet = new Set(getValues('tags'));
+    newTagSet.delete(tag);
+    setValue('tags', Array.from(newTagSet));
     // setProduct((prev) => ({
     //   ...prev,
     //   tags: prev.tags.filter((tag) => tag !== tagToRemove),
@@ -55,7 +57,10 @@ const onSubmit = (prodictLike: Product) => {
     // }
   };
 
-  const removeSize = (sizeToRemove: string) => {
+  const removeSize = (size: Size) => {
+    const sizeSet = new Set(getValues('sizes'));
+    sizeSet.delete(size);
+    setValue('sizes', Array.from(sizeSet));
     // setProduct((prev) => ({
     //   ...prev,
     //   sizes: prev.sizes.filter((size) => size !== sizeToRemove),
@@ -266,6 +271,12 @@ const onSubmit = (prodictLike: Product) => {
                        }`}
                     >
                       {size}
+                      <button 
+                        onClick={() => removeSize(size)}
+                        className='cursonr-pointer ml-2 text-blue-600 hover-text-blue-800 transition'
+                      >
+                        <X className='h-3 w-3' />
+                      </button>
                     </button>
                   ))}
                 </div>
@@ -280,7 +291,7 @@ const onSubmit = (prodictLike: Product) => {
 
               <div className="space-y-4">
                 <div className="flex flex-wrap gap-2">
-                  {product.tags.map((tag) => (
+                  {selectTags.map((tag) => (
                     <span
                       key={tag}
                       className="inline-flex items-center px-3 py-1 rounded-full text-sm font-medium bg-green-100 text-green-800 border border-green-200"
@@ -288,7 +299,7 @@ const onSubmit = (prodictLike: Product) => {
                       <Tag className="h-3 w-3 mr-1" />
                       {tag}
                       <button
-                        // onClick={() => removeTag(tag)}
+                        onClick={() => removeTag(tag)}
                         className="ml-2 text-green-600 hover:text-green-800 transition-colors duration-200"
                       >
                         <X className="h-3 w-3" />
@@ -300,16 +311,20 @@ const onSubmit = (prodictLike: Product) => {
                 <div className="flex gap-2">
                   <input
                     type="text"
-                    // value={newTag}
-                    // onChange={(e) => setNewTag(e.target.value)}
-                    // onKeyDown={(e) => e.key === 'Enter' && addTag()}
+                    ref={labelInputRef}
+                    onKeyDown={(e) => {
+                      if(e.key === 'Enter' || e.key === ' ' || e.key === ','){
+                        e.preventDefault();
+                        addTag();
+                        labelInputRef.current!.value = '';
+                      }
+                    }}
                     placeholder="Añadir nueva etiqueta..."
                     className="flex-1 px-4 py-2 border border-slate-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all duration-200"
                   />
-                  {/* TODO: */}
-                  {/* <Button onClick={addTag} className="px-4 py-2rounded-lg ">
+                  <Button onClick={addTag} className="px-4 py-2rounded-lg ">
                     <Plus className="h-4 w-4" />
-                  </Button> */}
+                  </Button> 
                 </div>
               </div>
             </div>
@@ -407,16 +422,17 @@ const onSubmit = (prodictLike: Product) => {
                   </span>
                   <span
                     className={`px-2 py-1 text-xs font-medium rounded-full ${
-                      product.stock > 5
+                     currentStock > 5
                         ? 'bg-green-100 text-green-800'
-                        : product.stock > 0
+                        : currentStock > 0
                         ? 'bg-yellow-100 text-yellow-800'
                         : 'bg-red-100 text-red-800'
                     }`}
                   >
-                    {product.stock > 5
+                    {currentStock} -
+                    {currentStock > 5
                       ? 'En stock'
-                      : product.stock > 0
+                      : currentStock > 0
                       ? 'Bajo stock'
                       : 'Sin stock'}
                   </span>
@@ -436,7 +452,7 @@ const onSubmit = (prodictLike: Product) => {
                     Tallas disponibles
                   </span>
                   <span className="text-sm text-slate-600">
-                    {product.sizes.length} tallas
+                    {selectSizes.length} tallas
                   </span>
                 </div>
               </div>
