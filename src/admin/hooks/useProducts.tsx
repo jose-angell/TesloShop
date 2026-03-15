863638eeafd941480fbc4ef8db9a09c67ed4e2ac
jose Angel <josegallardo.cor@gmail.com>
2026-03-14 22:51:18 -0600

    feat: accion de ceracion y actualizacion de producto

--- src/admin/hooks/useProducts.tsx
+++ src/admin/hooks/useProducts.tsx
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query"
+import { useMutation, useQuery } from "@tanstack/react-query"
 import { getProductByIdAction } from "../actions/get-product-by-id.action"
 import type { Product } from "@/interfaces/product.interface";
 
@@ -14,6 +14,9 @@ export const useProducts = (id: string) => {
   });
 
 //Todo: mutacion 
+//const [is] = useMutation()
+
+// TODO: or eliminar
   const handleSubmtFrom = async (productLike: Partial<Product>) => {
     console.log(productLike)
   }
