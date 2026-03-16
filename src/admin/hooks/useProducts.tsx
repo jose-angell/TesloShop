8a7588d2a252f0d5aa71b8ab29bd46576437de4c
jose Angel <josegallardo.cor@gmail.com>
2026-03-15 21:19:25 -0600

    feat: crear producto y majenar cambios

--- src/admin/hooks/useProducts.tsx
+++ src/admin/hooks/useProducts.tsx
@@ -1,11 +1,13 @@
-import { useMutation, useQuery } from "@tanstack/react-query"
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
 import { getProductByIdAction } from "../actions/get-product-by-id.action"
 import { createUpdateProductAction } from "../actions/create-update-product.action";
+import type { Product } from "@/interfaces/product.interface";
 
 
 
 
 export const useProducts = (id: string) => {
+  const queryClient = useQueryClient()
   const query = useQuery({
     queryKey: ['product', {id}],
     queryFn: () => getProductByIdAction(id),
@@ -13,10 +15,14 @@ export const useProducts = (id: string) => {
     staleTime: 1000 * 60 * 5, 
   });
 
-const mutation = useMutation({
+const mutation = useMutation({ //actualizacion de la informacion mastrada en la tablas con los nuevos datos
   mutationFn: createUpdateProductAction,
-  onSuccess: () => {
-    console.log('mutacion')
+  onSuccess: (product: Product) => {
+    // invalidar cache
+    queryClient.invalidateQueries({queryKey: ['products']})
+    queryClient.invalidateQueries({queryKey: ['product', {id: product.id}]})
+    // actualizar queryData
+    queryClient.setQueryData(['productos',{id: product.id}], product)
   }
 })
 
--- src/admin/pages/products/AdminProductsPage.tsx
+++ src/admin/pages/products/AdminProductsPage.tsx
@@ -39,7 +39,7 @@ export const AdminProductsPage = () => {
         <TableBody>
           {
             data?.products.map(product => (
-              <TableRow>
+              <TableRow key={product.id}>
               <TableCell><img src={product.images[0]} alt="Producto" className='w-20 h-20 object-cover rounded-md'/></TableCell>
               <TableCell>
                 <Link to={`/admin/products/${product.id}`} className='hover:text-blue-500 underline'>{product.title}</Link>
